6214454af54441367fd852bcf64169f15cb4d754
hds0005 <35776120+hds0005@users.noreply.github.com>
2019-03-27 20:02:58 -0500

    Fixed banner for users without saved locations

--- src/app/dashboard/past-weather-locations/past-weather-locations.component.ts
+++ src/app/dashboard/past-weather-locations/past-weather-locations.component.ts
@@ -7,15 +7,31 @@ import { WeatherService } from 'src/app/services/weather.service';
   styleUrls: ['./past-weather-locations.component.css']
 })
 export class PastWeatherLocationsComponent implements OnInit{
-  savedLocationData;
+  savedLocationData = [];
+  hasSaved: boolean;
 
   constructor(private weatherService: WeatherService) { }
 
   ngOnInit() {
+
     // setting the saved weather data
     this.weatherService.setSavedLocationData()
 
     // getting the saved weather data to be displayed
     this.savedLocationData = this.weatherService.getSavedLocationData() 
+
+    // workaround for delayed conditional rendering 
+    setTimeout(() => {
+      this.checkIfSaved()
+    }, 500)
+
+  }
+
+  checkIfSaved() {
+    if (this.savedLocationData.length !== 0) {
+      this.hasSaved = true
+    } else {
+      this.hasSaved = false
+    }
   }
 }
